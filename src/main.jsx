0e5f410abbae9072e3a843d843effea47208d537
chti24052-netizen <chti24052@g.nihon-u.ac.jp>
2025-12-11 15:18:43 +0900

    aa

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,11 @@
-import { createRoot } from "react-dom/client";
-import App from "./App";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css"; // Tailwind を使う場合は必要
 
-createRoot(document.querySelector("#content")).render(<App />);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+<React.StrictMode>
+<App />
+</React.StrictMode>
+);
